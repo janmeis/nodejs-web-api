c17743657e44c6ac62c8a81cc01a876c36cbb5b5
Jan Meissner <janmeis@yahoo.co.uk>
2025-01-15 14:54:43 +0100

    3rd commit

--- src/controllers/synoApiController.ts
+++ src/controllers/synoApiController.ts
@@ -44,13 +44,15 @@ export class SynoApiController {
   public static async folder(req: Request, res: Response): Promise<void> {
     try {
       const dirId = req.query.dirId || null;
-      const sortBy = req.query.sortBy || 'name';
+      const sortBy = req.query.sortBy || 'title';
       const sortDirection = req.query.sortDirection || 'asc';
       const limit = req.query.limit || config.get<string>('synoUrl.limit');
+      const offset = req.query.offset || 0;
 
       let url = `${SynoApiController.baseUrl}/AudioStation/folder.cgi?api=SYNO.AudioStation.Folder&version=3&method=list`
         + `&_sid=${SynoApiController.sid}`
         + `&limit=${limit}`
+        + `&offset=${offset}`
         + `&sort_by=${sortBy}`
         + `&sort_direction=${sortDirection}`
         + `&additional=${encodeURIComponent('song_tag,song_audio,song_rating')}`;
@@ -69,6 +71,107 @@ export class SynoApiController {
         return folder;
       });
       res.json(folders);
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+      res.status(500).json({ error: errorMessage });
+    }
+  }
+
+  public static async artist(req: Request, res: Response): Promise<void> {
+    try {
+      const sortBy = req.query.sortBy || 'name';
+      const sortDirection = req.query.sortDirection || 'asc';
+      const limit = req.query.limit || config.get<string>('synoUrl.limit');
+      const offset = req.query.offset || 0;
+      const filter = req.query.filter || null;
+     
+      let url = `${SynoApiController.baseUrl}/AudioStation/artist.cgi?api=SYNO.AudioStation.Artist&version=4&method=list`
+        + `&_sid=${SynoApiController.sid}`
+        + `&limit=${limit}`
+        + `&offset=${offset}`
+        + `&sort_by=${sortBy}`
+        + `&sort_direction=${sortDirection}`
+
+      if (filter)
+        url += `&filter=${encodeURIComponent(`${filter}`)}`;
+      console.log(url);
+
+      const response = await axios.get(url);
+      const artists = (response.data.data.artists as any[]).map((item) => item.name);
+      res.json(artists);
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+      res.status(500).json({ error: errorMessage });
+    }
+  }
+
+  public static async album(req: Request, res: Response): Promise<void> {
+    try {
+      const artist = req.query.artist || '';
+      const sortBy = req.query.sortBy || 'year';
+      const sortDirection = req.query.sortDirection || 'asc';
+      const limit = req.query.limit || config.get<string>('synoUrl.limit');
+      const offset = req.query.offset || 0;
+      const filter = req.query.filter || null;
+     
+      let url = `${SynoApiController.baseUrl}/AudioStation/album.cgi?api=SYNO.AudioStation.Album&version=3&method=list`
+        + `&_sid=${SynoApiController.sid}`
+        + `&limit=${limit}`
+        + `&offset=${offset}`
+        + `&sort_by=${sortBy}`
+        + `&sort_direction=${sortDirection}`;
+
+      if (artist)
+        url += `&artist=${encodeURIComponent(`${artist}`)}`;
+      if (filter)
+        url += `&filter=${encodeURIComponent(`${filter}`)}`;
+      console.log(url);
+
+      const response = await axios.get(url);
+      const albums = (response.data.data.albums as any[]).map((item) => `${item.display_artist}, ${item.name}, ${item.year}`);
+      res.json(albums);
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+      res.status(500).json({ error: errorMessage });
+    }
+  }
+
+  public static async song(req: Request, res: Response): Promise<void> {
+    try {
+      const artist = req.query.artist || '';
+      const album = req.query.album || '';
+      const sortBy = req.query.sortBy || 'year';
+      const sortDirection = req.query.sortDirection || 'asc';
+      const limit = req.query.limit || config.get<string>('synoUrl.limit');
+      const offset = req.query.offset || 0;
+      const filter = req.query.filter || null;
+     
+      let url = `${SynoApiController.baseUrl}/AudioStation/song.cgi?api=SYNO.AudioStation.Song&version=3&method=list`
+        + `&_sid=${SynoApiController.sid}`
+        + `&limit=${limit}`
+        + `&offset=${offset}`
+        + `&sort_by=${sortBy}`
+        + `&sort_direction=${sortDirection}`
+        + `&additional=${encodeURIComponent('song_tag,song_audio,song_rating')}`;
+
+      if (artist)
+        url += `&artist=${encodeURIComponent(`${artist}`)}`;
+      if (album)
+        url += `&album=${encodeURIComponent(`${album}`)}`;
+      if (filter)
+        url += `&filter=${encodeURIComponent(`${filter}`)}`;
+      console.log(url);
+
+      const response = await axios.get(url);
+      const songs = (response.data.data.songs as any[]).map((item) => {
+        let folder: IFolder = { id: item.id, title: item.title };
+        if (item.additional && item.additional.song_audio && item.additional.song_tag) {
+          const song = SynoApiController.populateSong(item.additional);
+          folder = { ...folder, ...song };
+        }
+        return folder;
+      });
+      res.json(songs);
     } catch (error) {
       const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
       res.status(500).json({ error: errorMessage });
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -55,7 +55,7 @@ export const setRoutes = (app: Express) => {
    *         name: sortBy
    *         schema:
    *           type: string
-   *           default: name
+   *           default: title
    *         description: The field to sort by
    *       - in: query
    *         name: sortDirection
@@ -85,4 +85,165 @@ export const setRoutes = (app: Express) => {
    *                     type: string
    */
   app.get('/syno/folder', SynoApiController.folder);
+
+  /**
+   * @swagger
+   * /syno/artist:
+   *   get:
+   *     summary: Get artist information
+   *     parameters:
+   *       - in: query
+   *         name: filter
+   *         schema:
+   *           type: string
+   *         description: Filter for the artist list
+   *       - in: query
+   *         name: sortBy
+   *         schema:
+   *           type: string
+   *           default: name
+   *         description: The field to sort by
+   *       - in: query
+   *         name: sortDirection
+   *         schema:
+   *           type: string
+   *           default: asc
+   *         description: The direction to sort (asc or desc)
+   *       - in: query
+   *         name: limit
+   *         schema:
+   *           type: integer
+   *           default: 20000
+   *         description: The maximum number of items to return
+   *     responses:
+   *       200:
+   *         description: Artist information
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 type: string
+   */
+  app.get('/syno/artist', SynoApiController.artist);
+
+  /**
+   * @swagger
+   * /syno/album:
+   *   get:
+   *     summary: Get album information
+   *     parameters:
+   *       - in: query
+   *         name: filter
+   *         schema:
+   *           type: string
+   *         description: Filter for the album list
+   *       - in: query
+   *         name: artist
+   *         schema:
+   *           type: string
+   *           default: Andrew Hill
+   *         description: The artist to list albums for
+   *       - in: query
+   *         name: sortBy
+   *         schema:
+   *           type: string
+   *           default: year
+   *         description: The field to sort by
+   *       - in: query
+   *         name: sortDirection
+   *         schema:
+   *           type: string
+   *           default: asc
+   *         description: The direction to sort (asc or desc)
+   *       - in: query
+   *         name: limit
+   *         schema:
+   *           type: integer
+   *           default: 20000
+   *         description: The maximum number of items to return
+   *     responses:
+   *       200:
+   *         description: Album information
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 type: object
+   *                 properties:
+   *                   id:
+   *                     type: string
+   *                   title:
+   *                     type: string
+   */
+  app.get('/syno/album', SynoApiController.album);
+
+  /**
+   * @swagger
+   * /syno/song:
+   *   get:
+   *     summary: Get song information
+   *     parameters:
+   *       - in: query
+   *         name: filter
+   *         schema:
+   *           type: string
+   *         description: Filter for the song list
+   *       - in: query
+   *         name: artist
+   *         schema:
+   *           type: string
+   *         description: The artist of the songs to list
+   *       - in: query
+   *         name: album
+   *         schema:
+   *           type: string
+   *         description: The album of the songs to list
+   *       - in: query
+   *         name: sortBy
+   *         schema:
+   *           type: string
+   *           default: year
+   *         description: The field to sort by
+   *       - in: query
+   *         name: sortDirection
+   *         schema:
+   *           type: string
+   *           default: asc
+   *         description: The direction to sort (asc or desc)
+   *       - in: query
+   *         name: limit
+   *         schema:
+   *           type: integer
+   *           default: 20000
+   *         description: The maximum number of items to return
+   *     responses:
+   *       200:
+   *         description: Song information
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 type: object
+   *                 properties:
+   *                   id:
+   *                     type: string
+   *                   title:
+   *                     type: string
+   *                   artist:
+   *                     type: string
+   *                   album:
+   *                     type: string
+   *                   year:
+   *                     type: integer
+   *                   genre:
+   *                     type: string
+   *                   duration:
+   *                     type: integer
+   *                   filesize:
+   *                     type: integer
+   */
+  app.get('/syno/song', SynoApiController.song);
 };
